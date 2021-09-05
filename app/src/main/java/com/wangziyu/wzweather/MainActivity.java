807dd27fbb1bd99523603c63877c433ef5bbdb09
王子钰 <13005922240@163.com>
2021-09-05 10:14:13 +0800

    项目框架

---
+++ app/src/main/java/com/wangziyu/wzweather/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wangziyu.wzweather;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * describe:
+ * author: wangziyu
+ * date: 2021/9/5
+ **/
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
